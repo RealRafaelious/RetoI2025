3547a651a60076edd2415c5cbbedc749dce70b50
Bruno Arias <ariasssb@gmail.com>
2025-07-23 07:26:33 -0300

    feat (limpiar centros)

--- Limpieza/LimpiarCentros.py
+++ Limpieza/LimpiarCentros.py
@@ -1,30 +1,31 @@
+from queue import Full
 import pandas as pd
 
 # ruta
-archivo = "C:/Users/Estudiante UCU/Repositorios/RetoI2025/Tabla_estudiantes_7moa9no.xlsx"
+centrosxlsx = "c:\Users\arias\Desktop\RETO1\Reto 1 - Datos - Ceibal\Datos\Tabla_centros_7moa9no.xlsx"
 
 # cargar
-df = pd.read_excel(archivo)
+df = pd.read_excel(centrosxlsx)
 
 # noromalizar nombres de columnas
 df.columns = [col.strip().lower().replace(" ", "_") for col in df.columns]
 
 
 # dividir entre duplicados y no duplicados
-duplicados = df[df.duplicated(subset="id_unico", keep=False)]
-no_duplicados = df[~df.duplicated(subset="id_unico", keep=False)]
+duplicados = df[df.duplicated(subset="id_centro", keep=False)]
+no_duplicados = df[~df.duplicated(subset="id_centro", keep=False)]
 
-# agrupar duplicados por id_unico
-grupos = duplicados.groupby("id_unico")
+# agrupar duplicados por id_centro
+grupos = duplicados.groupby("id_centro")
 
-# conservar solo los que tienen al menos un grupo válido
-duplicados_limpios = grupos.filter(lambda g: (g["grupo"] != "desconocido").any())
+# conservar solo los que tienen media de lvsMedia existente
+duplicados_limpios = grupos.filter(lambda g: (g["lvsMedia"] != "" and g["lvsMedia"] is not None ).any())
 
 # eliminar solo los que tienen grupo desconocido
-duplicados_limpios = duplicados_limpios[duplicados_limpios["grupo"] != "desconocido"]
+duplicados_limpios = duplicados_limpios[duplicados_limpios["lvsMedia"] !=  "" and g["lvsMedia"] is not None]
 
 # ids que solo tienen "desconocido" y guardarlos aparte
-ids_todos_desconocidos = grupos.filter(lambda g: (g["grupo"] == "desconocido").all())
+ids_todos_desconocidos = grupos.filter(lambda g: (g["lvsMedia"] != "" and g["lvsMedia"] is not None ).all())
 
 # todo lo limpio
 df_final = pd.concat([no_duplicados, duplicados_limpios], ignore_index=True)
