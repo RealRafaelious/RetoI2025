550ce21b69ea493c13b3f0b1ba169695a9f2275f
Bruno Arias <ariasssb@gmail.com>
2025-07-23 13:25:11 -0300

    build n start(): pregunta 1

--- Limpieza/Pregunta1/ConexionPorGradoEducativo.py
+++ Limpieza/Pregunta1/ConexionPorGradoEducativo.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # ruta
-archivo = "TablasActuales/Tabla_estudiantes_7moa9no.xlsx"
+excel_estudiantes = "TablasActuales/Tabla_estudiantes_7moa9no.xlsx"
 
 # cargar
-df = pd.read_excel(archivo)
+df_estudiantes = pd.read_excel(excel_estudiantes)
 
-# noromalizar nombres de columnas
-df.columns = [col.strip().lower().replace(" ", "_") for col in df.columns]
+# noromalizar nombres de columnas  
+df_estudiantes.columns = [col.strip().lower().replace(" ", "_") for col in df_estudiantes.columns]  
 
 ###############################################################################################
 # 1. ¿Varía el uso de la plataforma CREA según el grado educativo? ¿Hay diferencias por sexo? #
@@ -19,4 +19,12 @@ df.columns = [col.strip().lower().replace(" ", "_") for col in df.columns]
    # Sexo: Masculino, Femenino
    # Se tomaron en cuenta dentro de la muestra aquellas personas que se hayan conectado 30 o más veces.
    
+   
+df_estudiantes.groupby('grado')['cr_total_dias_ingreso'].mean()
+
+df_estudiantes.groupby('sexo')['cr_total_dias_ingreso'].mean()
  
+df_estudiantes.groupby(['grado', 'sexo'])['cr_total_dias_ingreso'].mean()
+
+
+
