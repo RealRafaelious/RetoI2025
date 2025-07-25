7c1f180d55f42e5d8cad7805ba8a6d2740c26838
Bruno Arias <ariasssb@gmail.com>
2025-07-25 12:40:51 -0300

    df

--- Preguntas/Pregunta4.py
+++
@@ -1,9 +0,0 @@
-
-################################################################################
-# 4. ¿Los estudiantes que comparten docente muestran patrones de uso similares?#
-################################################################################
-
-# Detalles de estándares:
-# Grado educativo: 7mo, 8vo, 9no
-# Sexo: Masculino, Femenino
-# Se tomaron en cuenta dentro de la muestra aquellas personas que se hayan conectado 30 o más veces.
